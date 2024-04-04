59ad045770274ab2f47385ec2935f820fa6d06ef
Henri Koski <henri.t.koski@gmail.com>
2024-04-04 22:15:43 +0300

    Enable extras and set theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- { "folke/tokyonight.nvim", lazy = true,opts = { style = "storm" }},
+  -- { "jacoborus/tender.vim" },
+  -- { "tomasiser/vim-code-dark" },
+  -- { "rmehri01/onenord.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin-frappe",
+      colorscheme = "nordic",
+    },
+  },
+}
